9a6ff2d945c8231cd4e6a305f71dafe0e16477d9
Joel Guilarte <jguilarte01@gmail.com>
2021-02-14 11:10:03 -0500

    Feat/e2e tests (#16)
    * fix(test): fixed execution of unit tests and e2e tests.
    
    * ci(lint): adding lint step to ci workflow.
    
    * ci(test): adding unit test execution to ci workflow.
    
    * ci(build): renaming build step in ci workflow.
    
    * ci(lint): integrated prettier with commit hooks and ng lint.
    
    * fix(syntax): fixed whitespace issues in package.json
    
    * fix(hooks): properly configuring husky hooks.
    
    * fix(hooks): properly configuring husky hooks.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
+import { Constants } from './app.constants';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -8,9 +9,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display my name', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('web app is running!');
+    expect(page.getTitleText()).toEqual(Constants.TITLE);
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -1,23 +1,42 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 module.exports = function (config) {
   config.set({
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--headless',
+          '--disable-gpu',
+          '--disable-translate',
+          '--disable-extensions'
+        ]
+      }
+    },
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/web'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+    coverageReporter: {
+      dir: require('path').join(__dirname, '<%= relativePathToWorkspaceRoot %>/coverage/<%= appName%>'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ],
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
@@ -25,8 +44,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    singleRun: true,
     restartOnFileChange: true
   });
 };
