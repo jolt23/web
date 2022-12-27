a2111ab1159a14c61d0bbe039dadbfee8fab3fe4
Joel Guilarte <jguilarte01@gmail.com>
2022-12-27 15:54:33 -0500

    build(codecoverage): adding codecoverage to npm test

--- karma.conf.js
+++ karma.conf.js
@@ -31,10 +31,7 @@ module.exports = function (config) {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageReporter: {
-      dir: require('path').join(
-        __dirname,
-        '<%= relativePathToWorkspaceRoot %>/coverage/<%= appName%>'
-      ),
+      dir: require('path').join(__dirname, './coverage/web'),
       subdir: '.',
       reporters: [{ type: 'html' }, { type: 'text-summary' }],
       fixWebpackSourcePaths: true,
