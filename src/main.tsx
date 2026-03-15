9258b5ede5a1a578777d2dd33833fdcf3bf4ad36
Joel Guilarte <jguilarte01@gmail.com>
2026-03-15 18:23:00 -0400

    upgraded vite/sass and updated look and feel for page2 on small screens

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.scss'
-import './app/app.component.scss'
 
 const container = document.getElementById('root')
 if (!container) throw new Error('Root container missing in index.html')
